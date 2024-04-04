9254fee26069548febe015d085e11a516a4bce91
cwan42 <vboxuser@UbuntuOS.myguest.virtualbox.org>
2024-04-04 10:30:29 +0800

    Updated a2b and initsort.c

--- 06_push_swap/srcs/a2b.c
+++ 06_push_swap/srcs/a2b.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 13:24:18 by cwan              #+#    #+#             */
-/*   Updated: 2024/04/02 15:34:23 by cwan             ###   ########.fr       */
+/*   Updated: 2024/04/04 10:28:15 by cwan42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 void	a2b(t_stack **a, t_stack **b)
 {
+	t_stack	*topush;
 	t_stack	*tgt;
 
+	topush = *a;
 	tgt = tgtb(b, cheapest(b, a));
-	if (tgt == *b)
-		pb(a, b);
-	else if (tgt->nu >= mediannode(b)->nu)
-		rb(b);
-	else
-		rrb(b);
-/*	else if (nodepos(b, tgt) <= (stacksize(b) / 2))
-		rb(b);
-	else if (nodepos(b, tgt) > (stacksize(b) / 2))
-		rrb(b);
-	else
-		pb(a, b);*/
-	printloops(a, b);
-	ft_printf("Tgt is %d, Nodeposb is %d\n",tgt->nu, nodepos(b, tgt));
+	while (topush != *b)
+	{
+		if (tgt == *b)
+			pb(a, b);
+		else if (nodepos(b, tgt) <= (stacksize(b) / 2))
+			rb(b);
+		else if (nodepos(b, tgt) > (stacksize(b) / 2))
+			rrb(b);
+	}
 }
