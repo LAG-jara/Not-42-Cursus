894c2860dfd62ac445149266be68f09d7f50b1f3
cwan <cwan@c1r8s2.42singapore.sg>
2024-04-14 16:05:32 +0800

    Updated testscript2.sh to use  instead of 1.5)

--- 06_push_swap/testscript2.sh
+++ 06_push_swap/testscript2.sh
@@ -6,7 +6,7 @@ for i in 1 2 3 4 5; do
 	./push_swap $ARG | ./checker_linux $ARG
 done
 
-for i in 1 2 3 4 5; do
+for i in $(seq 1 5); do
 	ARG=$(shuf -i 1-1000 -n 500 | tr '\n' ' ')
 	echo -n "$i: For 500 numbers: "
 	./push_swap $ARG | wc -l | tr -d '\n'
