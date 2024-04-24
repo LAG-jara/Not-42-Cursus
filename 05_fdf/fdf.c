b47f735d3930d9dea9e4bff99304486715eb5d0d
cwan <cwan@c1r8s4.42singapore.sg>
2024-04-24 15:54:52 +0800

    Updated fdf.c to trim, now need to center projection and add lines using either DDA or bresenham's

--- 05_fdf/fdf.c
+++ 05_fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 12:24:15 by cwan              #+#    #+#             */
-/*   Updated: 2024/04/24 15:42:09 by cwan             ###   ########.fr       */
+/*   Updated: 2024/04/24 15:52:52 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ void	drawstuff(t_mlx *fdf, int **map, char *av)
 	int		z;
 
 	y = 0;
-	theta = 30 * M_PI / 180;
+	theta = 45 * M_PI / 180;
 	while (map[y])
 	{
 		x = 0;
@@ -83,30 +83,6 @@ float	ft_abs(int n)
 		return (n);
 }
 /*
-void drawstuff(t_mlx *fdf, int **map, char *av)
-{
-  int x;
-  int y;
-  double x_rot;
-  double y_rot;
-  double theta = 30.0 * M_PI / 180;
-  double isometric_scale_x = 0.7;
-  double isometric_scale_y = 0.7;
-  double depth_scale = 0.5;
-
-  int max_y = numrow(av); // Assuming a function to get number of rows
-  int max_x = numcol(av); // Assuming a function to get number of columns
-
-  y = 0;
-  while (y < max_y - 1) { // Iterate until second-last row
-    x = 0;
-    while (x < max_x - 1) { // Iterate until second-last column
-      int z = map[y][x]; // Current point's z-value
-
-      // Calculate rotated coordinates for current point
-      x_rot = (x * cos(theta) - y * sin(theta)) * isometric_scale_x;
-      y_rot = (x * sin(theta) + y * cos(theta)) * isometric_scale_y - z * depth_scale;
-
       // Check for valid neighbors (adjust conditions based on your map dimensions)
       if (y + 1 < max_y) {
         // Calculate neighbor's rotated coordinates (downward neighbor)
@@ -118,15 +94,6 @@ void drawstuff(t_mlx *fdf, int **map, char *av)
           double y_step = y_rot + ((neighbor_y_rot - y_rot) / ft_abs(y_rot - neighbor_y_rot)) * i;
           mlx_pixel_put(fdf->ptr, fdf->win, (x_rot * 10) + WIDTH / 2,
                         y_step + HEIGHT / 2, 0xFFFFFF);
-        }
-      }
-
-      // Add similar logic to check and draw lines for right neighbor (x+1)
-
-      x++;
-    }
-    y++;
-  }
 }*/
 
 int	main(int ac, char *av[])
