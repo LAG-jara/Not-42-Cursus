070158dee9a51b4d25147c3633967b96adfe028d
cwan <cwan@c1r8s4.42singapore.sg>
2024-05-03 13:48:58 +0800

    Shifted fdfvalues over to utils.c temporarily

--- 05_fdf/srcs/utils.c
+++ 05_fdf/srcs/utils.c
@@ -6,11 +6,20 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:43:13 by cwan              #+#    #+#             */
-/*   Updated: 2024/04/29 14:21:53 by cwan             ###   ########.fr       */
+/*   Updated: 2024/05/03 12:09:56 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	fdfvalues(t_mlx *fdf)
+{
+	fdf->theta = 30 * M_PI / 180;
+	if (fdf->rows < fdf->cols)
+		fdf->scale = ((double)1000 / fdf->rows) / 2;
+	else
+		fdf->scale = ((double)1200 / fdf->cols) / 2;
+}
 
 float	ft_abs(int n)
 {
